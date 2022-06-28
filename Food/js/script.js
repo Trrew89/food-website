771f64d2b3ec6c4680f4765b0d5f63cb9d664315
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-06-28 12:24:27 +0200

    json server added

--- Food/js/script.js
+++ Food/js/script.js
@@ -152,7 +152,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    //window.addEventListener('scroll', showModalByScroll);
+    window.addEventListener('scroll', showModalByScroll);
     
 
 
@@ -238,29 +238,30 @@ window.addEventListener('DOMContentLoaded', function() {
             margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json');
-            const formData = new FormData(form);
-
+           const formData = new FormData(form);
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-            request.addEventListener('load', ()=>{
-                if(request.status === 200){
-                    console.log(request.response);
-                    showThanksModal(message.succes);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server1.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.succes);
+                
+                statusMessage.remove();
+            }).catch(()=>{
+                showThanksModal(message.failure);
+            }).finally(()=> {
+                form.reset();
             });
         });
     }
@@ -289,5 +290,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 4000);
     }
 
-
+    fetch('http://localhost:3000/menu')
+    .then(data=> data.json())
+    .then(res=> console.log(res));
 });
