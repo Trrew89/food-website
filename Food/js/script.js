b07fae45c8c324a53426c37105bf04894d5cebcf
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-06-30 14:19:20 +0200

    first try adding sliders and it's index

--- Food/js/script.js
+++ Food/js/script.js
@@ -197,38 +197,55 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    new MenuCard(
-        'img/tabs/vegy.jpg',
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item',
-        'big'
-    ).render();
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container",
-        'menu__item',
-        'big'
-    ).render();
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container",
-        'menu__item',
-        'big'
-    ).render();
+        return await res.json();
+    };
+
+    /*
+    getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title ,descr, price}) =>{
+            new MenuCard(img, altimg, title ,descr, price, '.menu .container').render();
+        });
+    });
+*/
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+
+    //         element.classList.add("menu__item");
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector(".menu .container").append(element);
+    //     });
+    // }
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title ,descr, price}) =>{
+                new MenuCard(img, altimg, title ,descr, price, '.menu .container').render();
+            });
+        });
+
+
 
     // Forms
 
@@ -239,9 +256,21 @@ window.addEventListener('DOMContentLoaded', function() {
         failure: 'Something went wrong'
     };
     forms.forEach(item =>{
-        postData(item);
+        bindpostData(item);
     });
-    function postData(form){
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+        return await res.json();
+    };
+
+    function bindpostData(form){
         form.addEventListener('submit', (e)=>{
             e.preventDefault();
 
@@ -254,19 +283,10 @@ window.addEventListener('DOMContentLoaded', function() {
             form.insertAdjacentElement('afterend', statusMessage);
 
            const formData = new FormData(form);
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+           
+           const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server1.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.succes);
@@ -304,7 +324,67 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 4000);
     }
 
-    fetch('http://localhost:3000/menu')
-    .then(data=> data.json())
-    .then(res=> console.log(res));
+    //Sliders
+    const slides = document.querySelectorAll('.offer__slide'),
+           prev = document.querySelector('.offer__slider-prev'),
+           next = document.querySelector('.offer__slider-next'),
+           total = document.querySelector('#total'),
+           current = document.querySelector('#current'),
+           slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+           slidesField = document.querySelector('.offer__slider-inner'),
+           width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    
+/*
+    slidesField.style.width = 100 * slides.length +'%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0,5s, all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide =>{
+        slide.style.width = width;
+    });
+
+*/
+    
+    showSlides(slideIndex);
+
+    if(slides.length<10){
+        total.textContent=`0${slides.length}`;
+    } else {
+        total.textContent=slides.length;
+    }
+
+    function showSlides(n){
+        if(n >slides.length){
+            slideIndex = 1;
+        }
+
+        if ( n< 1){
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex-1].style.display = 'block';
+
+        if(slideIndex < 10){
+            current.textContent=`0${slideIndex}`;
+        } else {
+            current.textContent=slideIndex;
+        }
+    }
+
+    function plusSLides(n){
+        showSlides(slideIndex +=n);
+    }
+
+
+    prev.addEventListener('click', ()=>{
+        plusSLides(-1);
+    });
+    next.addEventListener('click', ()=>{
+        plusSLides(+1);
+    });
+
 });
