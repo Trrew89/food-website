ac5609efcf56b721c5a153629def2893f86d2806
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-07-19 16:34:38 +0200

    added replce function instead of slice

--- Food/js/script.js
+++ Food/js/script.js
@@ -326,6 +326,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Sliders
     const slides = document.querySelectorAll('.offer__slide'),
+            slider = document.querySelector('.offer__slider'),
            prev = document.querySelector('.offer__slider-prev'),
            next = document.querySelector('.offer__slider-next'),
            total = document.querySelector('#total'),
@@ -356,11 +357,57 @@ window.addEventListener('DOMContentLoaded', function() {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
+    
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i =0; i < slides.length; i++){
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i+1);
+        //dot.classList.add('.dot');
+        dot.style.cssText= `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i ==0){
+            dot.style.opacity = 1;
+        } 
+        indicators.append(dot);
+        dots.push (dot);
+    }
+
+
     next.addEventListener('click', ()=>{
-        if (offset === +width.slice(0, width.length-2)* (slides.length - 1)){
+        if (offset === deleteNotDigits(width)* (slides.length - 1)){
             offset =0;
         } else {
-            offset += +width.slice(0, width.length-2);
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -370,18 +417,14 @@ window.addEventListener('DOMContentLoaded', function() {
             slideIndex++;
         }
 
-        if(slides.length < 10){
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-    });
+        changeIndexAndDots();
+    });    
 
     prev.addEventListener('click', ()=>{
         if (offset == 0){
-            offset = +width.slice(0, width.length-2)* (slides.length - 1);
+            offset = deleteNotDigits(width)* (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length-2);
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -391,12 +434,36 @@ window.addEventListener('DOMContentLoaded', function() {
             slideIndex --;
         }
 
+        changeIndexAndDots();
+    });
+
+    dots.forEach( dot=> {
+        dot.addEventListener('click', (e)=>{
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width)* (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            changeIndexAndDots();
+        });
+    });
+
+
+    function changeIndexAndDots(){
         if(slides.length < 10){
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
-    });
+
+        dots.forEach(dot => dot.style.opacity ='.5');
+        dots[slideIndex-1].style.opacity = 1; 
+    }
+
+    function deleteNotDigits(str){
+        return +str.replace(/\D/g, '');
+    }
 
     /*
     showSlides(slideIndex);
